c0db9778abc6d9ca096ce1c3336316b657e0c095
Emanuelortega <matias_ortega1996@outlook.com>
2022-10-10 16:43:43 -0300

    add boostrap, delete, edit method

--- src/main/java/ar/com/educationit/controllers/SociosController.java
+++ src/main/java/ar/com/educationit/controllers/SociosController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ar.com.educationit.domain.Socios;
 import ar.com.educationit.services.SociosService;
@@ -25,5 +27,20 @@ public class SociosController {
 		List<Socios> socios = this.sociosService.buscarTodos();
 		model.addAttribute("socios",socios);
 		return "socios";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(
+			@RequestParam(name = "idSocio", required = true) Long idSocios) {
+		this.sociosService.eliminar(idSocios);
+		return "redirect:/socio/all";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String preEdit(
+			@PathVariable(name = "id", required = true) Long id, Model model) {
+		Socios socios = this.sociosService.buscarSocio(id);
+		model.addAttribute("socio", socios);
+		return "edit";
 	}
 }
